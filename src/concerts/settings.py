51fe029e3241bbe3dda5564ad65cf908a8ee8c3f
ncolosso <colossonico@gmail.com>
2022-08-22 20:49:01 -0400

    Switched to Docker Postgres container for backend db

--- src/concerts/settings.py
+++ src/concerts/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Custom Apps
     'concertlistings',
 ]
 
@@ -75,9 +77,17 @@ WSGI_APPLICATION = 'concerts.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'concerts',
+        'USER': 'ncolosso',
+        'PASSWORD': 'django',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
