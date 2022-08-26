fb831ed4a24a4a0442ac6153dfce9e79be9129f9
emoltz <es5541@nyu.edu>
2022-08-26 10:56:26 -0400

    favicon added

--- src/static/javascript/script.js
+++ src/static/javascript/script.js
@@ -1,7 +1,9 @@
 //TODO if date is `SEP` then change to diff color, if `NOV` then change... etc.
 const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
 
-let cardMonth = document.querySelector('.date').textContent.slice(-3);
+let cardMonthText = document.querySelector('.date').textContent.slice(-3);
+let cardDate = document.querySelector('.date');
+let card = document.querySelector('.card-1');
 let cardDatesAll = document.querySelectorAll('.date');
 
 
@@ -10,9 +12,18 @@ let cardDatesAll = document.querySelectorAll('.date');
 
 function changeColor(s){
     let month = s.toUpperCase();
-    if(month === months[8]){
-        console.log("working")
+    if(month === months[8]){ //SEPTEMBER
+        cardDate.classList.toggle('accent-1');
+        cardDate.classList.toggle('accent-2');
+
+        card.classList.remove('card-1');
+        // console.log(card.classList);
+        card.classList.add('card-2');
     }
+    if(month === months[10]){ //OCTOBER
+
+    }
+    // ...etc
 }
 
-changeColor(cardMonth);
+changeColor(cardMonthText);
