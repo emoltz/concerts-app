9e8a797a9b938872a5612cece66aa9ab35c72ca1
ncolosso <colossonico@gmail.com>
2022-08-26 15:32:01 -0400

    Added API wrapper and re-sourced concerts list

--- src/concertlistings/urls.py
+++ src/concertlistings/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 
 from .views import (
+    concert_list_view,
+    concert_detail_view,
     ConcertListView,
-    ConcertDetailView
+    ConcertDetailView,
 )
 
 
 app_name = 'concerts'
 urlpatterns = [
-    path('', ConcertListView.as_view(), name='concert-list'),
-    path('<int:id>/', ConcertDetailView.as_view(), name='concert-detail')
+    path('', concert_list_view, name='concert-list'),
+    path('<str:id>/', concert_detail_view, name='concert-detail')
+    # path('G5vVZ9dLbWv5L/', concert_detail_view, name='concert-detail')
 ]
